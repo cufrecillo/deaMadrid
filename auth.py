4d92992972f194b425323046f99eaf9f089d4234
Roberto Martinez Camacho <50523913+cufrecillo@users.noreply.github.com>
2021-08-04 22:03:06 +0200

    opciones de admin para la creacion de usuario

--- auth.py
+++ auth.py
@@ -1,11 +1,16 @@
 import datetime
-from os import truncate
+import os
 import bcrypt
 import auth_de
 
 def auth():
+
+    CWD = os.path.dirname(__file__)
+
     rol_user = ["admin", "agent"]
+    contra_admin = "1234"
     back = True
+
     while back == True:
         auth_de.menu_usuarios()
         user = input("Opcion: ")
@@ -24,9 +29,22 @@ def auth():
                 print("El usuario ya existe")
             except ValueError:
                 user_pwd = input("Password: ")
-                auth_de.rol_options(rol_user)
-                option = int(input("Opcion: ")) -1
-                user_rol = rol_user[option]
+                rol_control = False
+                while rol_control == False:
+                    print("-----------------------")
+                    auth_de.rol_options(rol_user)
+                    option = int(input("Opcion: ")) -1
+                    if option == 1: # solo consultas // agente
+                        user_rol = rol_user[option]
+                        rol_control = True
+                    elif option == 0: # admin
+                        contra_user_admin = input("Introduzca contraseña de administrador: ")
+                        if contra_user_admin == contra_admin:
+                            print("Contraseña OK.")
+                            user_rol = rol_user[option]
+                            rol_control = True
+                        else:
+                            print("Contraseña erronea.")
                 users = auth_de.read_json("users.json")
                 new_user = {"name": user_name, "pwd": bcrypt.hashpw(user_pwd.encode(), bcrypt.gensalt()).decode(), "rol": user_rol, "user_since": datetime.date.today().isoformat()}
                 users["data"].append(new_user)
